99b5d0f37e81ae089d1ad463aaa93b3d75fbe581
colcham <88758562+colcham@users.noreply.github.com>
2022-08-17 21:32:59 -0700

    jj

---
+++ styles/DistributionSites_2_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_DistributionSites_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "11.700000000000001px \'Helvetica\', sans-serif";
+    var labelFill = "#515151";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Site") !== null) {
+        labelText = String(feature.get("Site"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 10.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(186,158,158,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
